b2d94441908a06fcf1d179039da495491222f638
rubenjm <sart.rubi98@gmail.com>
2024-12-13 12:01:06 -0600

    Refactor: Update SASS syntax from .scss to .sass and migrate from @import to @use
    - Converted all SASS files from .scss to .sass syntax for improved readability and adherence to updated styling guidelines.
    - Replaced deprecated @import statements with @use to align with the latest SASS module system, improving encapsulation and performance.
    - Updated references across all files to support the @use module structure, reducing namespace collisions and improving maintainability.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,18 @@
-const SHARE_BTN = document.querySelector('.share-btn')
-const SHARE_BTN_MOBILE = document.querySelector('.btn-mobile')
+const domElements = {
+	shareBtn: document.querySelector('.share-btn'),
+	mobileShareBtn: document.querySelector('.btn-mobile'),
+	tooltip: document.getElementById('tooltip'),
+}
 
-SHARE_BTN.addEventListener('click', () => {
-	let tooltip = document.getElementById('tooltip')
-
+domElements.shareBtn.addEventListener('click', () => {
 	if (true) {
-		tooltip.style.display = tooltip.style.display === 'flex' ? 'none' : 'flex'
+		domElements.tooltip.style.display = domElements.tooltip.style.display === 'flex' ? 'none' : 'flex'
 	}
 })
 
-// Event for the btn in the mobile.scss file
-SHARE_BTN_MOBILE.addEventListener('click', () => {
-	let tooltip = document.getElementById('tooltip')
-
+// Event for the btn in the mobile.sass file
+domElements.mobileShareBtn.addEventListener('click', () => {
 	if (true) {
-		tooltip.style.display = tooltip.style.display === 'flex' ? 'none' : 'flex'
+		domElements.tooltip.style.display = domElements.tooltip.style.display === 'flex' ? 'none' : 'flex'
 	}
 })
