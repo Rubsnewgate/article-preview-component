214a2b984207a6a634b36ab09fb9790a2c70cfff
rubsnewgate <sart.rubi98@gmail.com>
2024-12-19 21:02:54 -0600

    fix(tooltip): elimination of an unnecessary button in the mobile versionof the tooltip; also improves the JS file, but the click issue with the tooltip button in the mobile version persists

--- js/main.js
+++ js/main.js
@@ -1,18 +1,24 @@
 const domElements = {
-	shareBtn: document.querySelector('.share-btn'),
-	mobileShareBtn: document.querySelector('.btn-mobile'),
+	shareBtn: document.getElementById('share-btn'),
 	tooltip: document.getElementById('tooltip'),
+	userPicture: document.querySelector('.user__photo'),
+	userInfo: document.querySelector('.user__info'),
 }
 
-domElements.shareBtn.addEventListener('click', () => {
-	if (true) {
-		domElements.tooltip.style.display = domElements.tooltip.style.display === 'flex' ? 'none' : 'flex'
-	}
-})
+function toogleTooltip () {
+	domElements.tooltip.style.display = domElements.tooltip.style.display === 'flex' ? 'none' : 'flex'
+	const photo = domElements.userPicture
+	const info = domElements.userInfo
 
-// Event for the btn in the mobile.sass file
-domElements.mobileShareBtn.addEventListener('click', () => {
-	if (true) {
-		domElements.tooltip.style.display = domElements.tooltip.style.display === 'flex' ? 'none' : 'flex'
+	// hidden elements in mobile devices
+	if (photo.classList.contains('hidden')) {
+		photo.classList.remove('hidden')
+        info.classList.remove('hidden')
 	}
-})
+	else {
+        photo.classList.add('hidden')
+        info.classList.add('hidden')
+    }
+}
+
+domElements.shareBtn.addEventListener('click', () => toogleTooltip())
